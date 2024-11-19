83a201683a57fe8dc7aa5202b20183869cb53c34
GiadaMorena <160850161+GiadaMorena@users.noreply.github.com>
2024-11-19 21:10:19 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TreasureHunt from './TreasureHunt';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<TreasureHunt />);

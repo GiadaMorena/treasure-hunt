5344e2dc31b77770f4d50574d33f3abac2bcd512
GiadaMorena <160850161+GiadaMorena@users.noreply.github.com>
2024-11-19 21:24:21 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TreasureHunt from './TreasureHunt';
-import './styles.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<TreasureHunt />);
